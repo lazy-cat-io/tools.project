1bf3e360a2035d9060e646eebac4bacac6ca4818
Ilshat Sultanov <ilshat@sultanov.team>
2022-04-08 02:59:29 +0300

    Add helper functions `ensure-dir`, `ensure-file`

--- src/main/clojure/tools/path.clj
+++ src/main/clojure/tools/path.clj
@@ -1,5 +1,6 @@
 (ns tools.path
   (:require
+    [clojure.java.io :as io]
     [clojure.string :as str]
     [tools.system :as system])
   (:import
@@ -7,7 +8,8 @@
       File)))
 
 
-(def file-separator File/separator)
+(def file-separator
+  File/separator)
 
 
 (defn user-dir
@@ -19,3 +21,26 @@
   [sym]
   (some-> sym
           (str/replace "." file-separator)))
+
+
+(defn ensure-dir
+  ^File [dir]
+  (let [dir' (io/file dir)]
+    (if (.exists dir')
+      dir'
+      (if (.mkdirs dir')
+        dir'
+        (throw (ex-info (str "Can't create directory " dir) {:dir dir}))))))
+
+
+(defn ensure-file
+  ([file]
+   (ensure-file file ""))
+  ([file contents & opts]
+   (let [file (io/file file)
+         parent (.getParent file)]
+     (if (.exists (io/file parent))
+       (apply spit file contents opts)
+       (do
+         (ensure-dir parent)
+         (apply spit file contents opts))))))
